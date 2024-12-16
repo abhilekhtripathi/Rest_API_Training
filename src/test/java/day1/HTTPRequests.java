5fa33fd8bcb569087b09c9f6574e7b01fbde21cb
Abhilekh <abhilekh@saffire.com>
2024-12-16 17:44:30 +0530

    updated

--- src/test/java/day1/HTTPRequests.java
+++ src/test/java/day1/HTTPRequests.java
@@ -23,13 +23,11 @@ import io.restassured.http.ContentType;
  * 
  */
 public class HTTPRequests {
-
-	//@Test
-	void getUsers() {
-		
 	
-		
-		
+	int id;
+
+	@Test(priority = 1)
+	void getUsers() {
 		
 		when ()
 		
@@ -42,7 +40,7 @@ public class HTTPRequests {
 		   .log().all();
 	}
 	
-	@Test
+	@Test(priority = 2)
 	 public void getCreateUser() {
         // Prepare request body
         HashMap<String, String> hs = new HashMap<>();
@@ -61,4 +59,49 @@ public class HTTPRequests {
             .body("job", equalTo("Engineer")) 
             .log().all(); // Log full response for debugging
     }
-}
+	    @Test(priority = 3)
+	   public void updateUser() {
+	    	
+	    	
+	    	 HashMap<String, String> hs = new HashMap<>();
+	         hs.put("name", "ABHI");
+	         hs.put("job", " Automation Engineer");
+	         
+	         // Make POST request and validate response
+	         given()
+	             .contentType(ContentType.JSON)
+	             .body(hs)
+	         .when()
+	             .put("https://reqres.in/api/users" + id)
+	         .then()
+	             .statusCode(200) // Validate the response code
+	             .body("name", equalTo("pavan")) // Validate response body
+	             .body("job", equalTo("Engineer")) 
+	             .log().all(); // Log full response for debugging
+	    	
+	    	
+	    	
+	    	
+	    	
+	    }
+	
+	
+	
+	
+	@Test(priority = 4)
+	
+	public void deleteUser()
+	{
+		
+		given()
+		
+		.when()
+		 .delete("https://reqres.in/api/users/"+id)
+		.then()
+		 .statusCode(204)
+		 .log().all();
+		
+		
+		
+	}
+	}
